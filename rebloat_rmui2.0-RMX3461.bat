b095c8f0e581f0d668c03bbce9e7fda13c3c45c3
Guru <70903440+Guru-25@users.noreply.github.com>
2022-05-25 15:55:07 +0530

    Update rebloat_rmui2.0-RMX3461.bat

--- rebloat_rmui2.0-RMX3461.bat
+++ rebloat_rmui2.0-RMX3461.bat
@@ -23,96 +23,96 @@ echo Enabling Theme Store
 adb shell pm enable --user 0 com.heytap.themestore
 
 echo Reinstalling App Enhancement Services
-adb shell pm cmd package install-existing --user 0 com.coloros.gamespace
+adb shell pm install-existing --user 0 com.coloros.gamespace
 
 echo Reinstalling Assistant
-adb shell pm cmd package install-existing --user 0 com.google.android.apps.googleassistant
+adb shell pm install-existing --user 0 com.google.android.apps.googleassistant
 
 echo Reinstalling Calendar
-adb shell pm cmd package install-existing --user 0 com.google.android.calendar
+adb shell pm install-existing --user 0 com.google.android.calendar
 
 echo Reinstalling Chrome
-adb shell pm cmd package install-existing --user 0 com.android.chrome
+adb shell pm install-existing --user 0 com.android.chrome
 
 echo Reinstalling Compass
-adb shell pm cmd package install-existing --user 0 com.coloros.compass2
+adb shell pm install-existing --user 0 com.coloros.compass2
 
 echo Reinstalling Contacts
-adb shell pm cmd package install-existing --user 0 com.google.android.contacts
+adb shell pm install-existing --user 0 com.google.android.contacts
 
 echo Reinstalling Digital Wellbeing
-adb shell pm cmd package install-existing --user 0 com.google.android.apps.wellbeing
+adb shell pm install-existing --user 0 com.google.android.apps.wellbeing
 
 echo Reinstalling File Manager
-adb shell pm cmd package install-existing --user 0 com.coloros.filemanager
+adb shell pm install-existing --user 0 com.coloros.filemanager
 
 echo Reinstalling Files by Google
-adb shell pm cmd package install-existing --user 0 com.google.android.apps.nbu.files
+adb shell pm install-existing --user 0 com.google.android.apps.nbu.files
 
 echo Reinstalling FinShell Pay
-adb shell pm cmd package install-existing --user 0 com.finshell.fin
+adb shell pm install-existing --user 0 com.finshell.fin
 
 echo Reinstalling Gboard
-adb shell pm cmd package install-existing --user 0 com.google.android.inputmethod.latin
+adb shell pm install-existing --user 0 com.google.android.inputmethod.latin
 
 echo Reinstalling Glance for realme
-adb shell pm cmd package install-existing --user 0 com.glance.internet
+adb shell pm install-existing --user 0 com.glance.internet
 
 echo Reinstalling Gmail
-adb shell pm cmd package install-existing --user 0 com.google.android.gm
+adb shell pm install-existing --user 0 com.google.android.gm
 
 echo Reinstalling Google
-adb shell pm cmd package install-existing --user 0 com.google.android.googlequicksearchbox
+adb shell pm install-existing --user 0 com.google.android.googlequicksearchbox
 
 echo Reinstalling Google Assistant
-adb shell pm cmd package install-existing --user 0 com.android.hotwordenrollment.okgoogle
+adb shell pm install-existing --user 0 com.android.hotwordenrollment.okgoogle
 
 echo Reinstalling Google Contacts Sync
-adb shell pm cmd package install-existing --user 0 com.google.android.syncadapters.contacts
+adb shell pm install-existing --user 0 com.google.android.syncadapters.contacts
 
 echo Reinstalling Google Pay
-adb shell pm cmd package install-existing --user 0 com.google.android.apps.nbu.paisa.user
+adb shell pm install-existing --user 0 com.google.android.apps.nbu.paisa.user
 
 echo Reinstalling Hot Apps
-adb shell pm cmd package install-existing --user 0 com.opos.cs
+adb shell pm install-existing --user 0 com.opos.cs
 
 echo Reinstalling Keep Notes
-adb shell pm cmd package install-existing --user 0 com.google.android.keep
+adb shell pm install-existing --user 0 com.google.android.keep
 
 echo Reinstalling Lens
-adb shell pm cmd package install-existing --user 0 com.google.ar.lens
+adb shell pm install-existing --user 0 com.google.ar.lens
 
 echo Reinstalling Maps
-adb shell pm cmd package install-existing --user 0 com.google.android.apps.maps
+adb shell pm install-existing --user 0 com.google.android.apps.maps
 
 echo Reinstalling Messages
-adb shell pm cmd package install-existing --user 0 com.google.android.apps.messaging
+adb shell pm install-existing --user 0 com.google.android.apps.messaging
 
 echo Reinstalling Meta App Installer
-adb shell pm cmd package install-existing --user 0 com.facebook.system
+adb shell pm install-existing --user 0 com.facebook.system
 
 echo Reinstalling Meta App Manager
-adb shell pm cmd package install-existing --user 0 com.facebook.appmanager
+adb shell pm install-existing --user 0 com.facebook.appmanager
 
 echo Reinstalling Meta Services
-adb shell pm cmd package install-existing --user 0 com.facebook.services
+adb shell pm install-existing --user 0 com.facebook.services
 
 echo Reinstalling Phone
-adb shell pm cmd package install-existing --user 0 com.google.android.dialer
+adb shell pm install-existing --user 0 com.google.android.dialer
 
 echo Reinstalling Phone Manager
-adb shell pm cmd package install-existing --user 0 com.coloros.phonemanager
+adb shell pm install-existing --user 0 com.coloros.phonemanager
 
 echo Reinstalling Photos
-adb shell pm cmd package install-existing --user 0 com.google.android.apps.photos
+adb shell pm install-existing --user 0 com.google.android.apps.photos
 
 echo Reinstalling Video (System)
-adb shell pm cmd package install-existing --user 0 com.coloros.video
+adb shell pm install-existing --user 0 com.coloros.video
 
 echo Reinstalling Youtube
-adb shell pm cmd package install-existing --user 0 com.google.android.youtube
+adb shell pm install-existing --user 0 com.google.android.youtube
 
 echo Reinstalling realme Share
-adb shell pm cmd package install-existing --user 0 com.coloros.oshare
+adb shell pm install-existing --user 0 com.coloros.oshare
 
 echo Done
