d81bff4aa7851c153545b062419d3c1d4372e56a
Guru <70903440+Guru-25@users.noreply.github.com>
2022-06-10 13:46:24 +0000

    Update debloat_rmui2.0-RMX3461.bat

--- debloat_rmui2.0-RMX3461.bat
+++ debloat_rmui2.0-RMX3461.bat
@@ -1,147 +1,147 @@
-@echo off
-echo Running realme 9 5G Speed Edition debloat script
+echo "Running realme 9 5G Speed Edition debloat script"
 
-echo Disabling Browser
+echo "Disabling Browser"
 adb shell pm disable-user --user 0 com.heytap.browser
 
-echo Disabling HeyTap Cloud
+echo "Disabling HeyTap Cloud"
 adb shell pm disable-user --user 0 com.heytap.cloud
 
-echo Disabling Lock Screen Magazine
+echo "Disabling Lock Screen Magazine"
 adb shell pm disable-user --user 0 com.heytap.pictorial
 
-echo Disabling My realme
+echo "Disabling My realme"
 adb shell pm disable-user --user 0 com.heytap.usercenter
 
-echo Disabling Theme Store
+echo "Disabling Theme Store"
 adb shell pm disable-user --user 0 com.heytap.themestore
 
-echo Uninstalling Android Accessibility Suite
+echo "Uninstalling Android Accessibility Suite"
 adb shell pm uninstall -k --user 0 com.google.android.marvin.talkback
 
-echo Uninstalling Android Auto
+echo "Uninstalling Android Auto"
 adb shell pm uninstall -k --user 0 com.google.android.projection.gearhead
 
-echo Uninstalling Assistant
+echo "Uninstalling Assistant"
 adb shell pm uninstall -k --user 0 com.google.android.apps.googleassistant
 
-echo Uninstalling atlasService
+echo "Uninstalling atlasService"
 adb shell pm uninstall -k --user 0 com.oplus.atlas
 
-echo Uninstalling Calculator
+echo "Uninstalling Calculator"
 adb shell pm uninstall -k --user 0 com.coloros.calculator
 
-echo Uninstalling Calendar
+echo "Uninstalling Calendar"
 adb shell pm uninstall -k --user 0 com.google.android.calendar
 
-echo Uninstalling Carrier Services
+echo "Uninstalling Carrier Services"
 adb shell pm uninstall -k --user 0 com.google.android.ims
 
-echo Uninstalling Chrome
+echo "Uninstalling Chrome"
 adb shell pm uninstall -k --user 0 com.android.chrome
 
-echo Uninstalling Compass
+echo "Uninstalling Compass"
 adb shell pm uninstall -k --user 0 com.coloros.compass2
 
-echo Uninstalling Data Restore Tool
+echo "Uninstalling Data Restore Tool"
 adb shell pm uninstall -k --user 0 com.google.android.apps.restore
 
-echo Uninstalling Digital Wellbeing
+echo "Uninstalling Digital Wellbeing"
 adb shell pm uninstall -k --user 0 com.google.android.apps.wellbeing
 
-echo Uninstalling File Manager
+echo "Uninstalling File Manager"
 adb shell pm uninstall -k --user 0 com.coloros.filemanager
 
-echo Uninstalling Files by Google
+echo "Uninstalling Files by Google"
 adb shell pm uninstall -k --user 0 com.google.android.apps.nbu.files
 
-echo Uninstalling FinShell Pay
+echo "Uninstalling FinShell Pay"
 adb shell pm uninstall -k --user 0 com.finshell.fin
 
-echo Uninstalling Gboard
+echo "Uninstalling Gboard"
 adb shell pm uninstall -k --user 0 com.google.android.inputmethod.latin
 
-echo Uninstalling Glance for realme
+echo "Uninstalling Glance for realme"
 adb shell pm uninstall -k --user 0 com.glance.internet
 
-echo Uninstalling Gmail
+echo "Uninstalling Gmail"
 adb shell pm uninstall -k --user 0 com.google.android.gm
 
-echo Uninstalling Google
+echo "Uninstalling Google"
 adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
 
-echo Uninstalling Google Assistant
+echo "Uninstalling Google Assistant"
 adb shell pm uninstall -k --user 0 com.android.hotwordenrollment.okgoogle
 
-echo Uninstalling Google Pay
+echo "Uninstalling Google Pay"
 adb shell pm uninstall -k --user 0 com.google.android.apps.nbu.paisa.user
 
-echo Uninstalling HeySynergy
+echo "Uninstalling HeySynergy"
 adb shell pm uninstall -k --user 0 com.heytap.synergy
 
-echo Uninstalling Hot Apps
+echo "Uninstalling Hot Apps"
 adb shell pm uninstall -k --user 0 com.opos.cs
 
-echo Uninstalling Keep Notes
+echo "Uninstalling Keep Notes"
 adb shell pm uninstall -k --user 0 com.google.android.keep
 
-echo Uninstalling Lens
+echo "Uninstalling Lens"
 adb shell pm uninstall -k --user 0 com.google.ar.lens
 
-echo Uninstalling Maps
+echo "Uninstalling Maps"
 adb shell pm uninstall -k --user 0 com.google.android.apps.maps
 
-echo Uninstalling Messages
+echo "Uninstalling Messages"
 adb shell pm uninstall -k --user 0 com.google.android.apps.messaging
 
-echo Uninstalling Meta App Installer
+echo "Uninstalling Meta App Installer"
 adb shell pm uninstall -k --user 0 com.facebook.system
 
-echo Uninstalling Meta App Manager
+echo "Uninstalling Meta App Manager"
 adb shell pm uninstall -k --user 0 com.facebook.appmanager
 
-echo Uninstalling Meta Services
+echo "Uninstalling Meta Services"
 adb shell pm uninstall -k --user 0 com.facebook.services
 
-echo Uninstalling Music
+echo "Uninstalling Music"
 adb shell pm uninstall -k --user 0 com.heytap.music
 
-echo Uninstalling Payment Protection
+echo "Uninstalling Payment Protection"
 adb shell pm uninstall -k --user 0 com.coloros.securepay
 
-echo Uninstalling Phone
+echo "Uninstalling Phone"
 adb shell pm uninstall -k --user 0 com.google.android.dialer
 
-echo Uninstalling Phone Manager
+echo "Uninstalling Phone Manager"
 adb shell pm uninstall -k --user 0 com.coloros.phonemanager
 
-echo Uninstalling Photos
+echo "Uninstalling Photos"
 adb shell pm uninstall -k --user 0 com.google.android.apps.photos
 
-echo Uninstalling Private Safe
+echo "Uninstalling Private Safe"
 adb shell pm uninstall -k --user 0 com.coloros.encryption
 
-echo Uninstalling Secure Keyboard
+echo "Uninstalling Secure Keyboard"
 adb shell pm uninstall -k --user 0 com.coloros.securitykeyboard
 
-echo Uninstalling Secure Payment
+echo "Uninstalling Secure Payment"
 adb shell pm uninstall -k --user 0 com.nearme.atlas
 
-echo Uninstalling Sound Amplifier
+echo "Uninstalling SIM card apps"
+adb shell pm uninstall -k --user 0 com.android.stk
+
+echo "Uninstalling Sound Amplifier"
 adb shell pm uninstall -k --user 0 com.google.android.accessibility.soundamplifier
 
-echo Uninstalling Speech Services by Google
+echo "Uninstalling Speech Services by Google"
 adb shell pm uninstall -k --user 0 com.google.android.tts
 
-echo Uninstalling User Experience Program
+echo "Uninstalling User Experience Program"
 adb shell pm uninstall -k --user 0 com.nearme.statistics.rom
 
-echo Uninstalling Video (System)
+echo "Uninstalling Video (System)"
 adb shell pm uninstall -k --user 0 com.coloros.video
 
-echo Uninstalling Youtube
+echo "Uninstalling Youtube"
 adb shell pm uninstall -k --user 0 com.google.android.youtube
 
-echo Done
-
-pause
+echo "Done!"
