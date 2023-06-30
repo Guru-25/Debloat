cee1f3c228cab18bafae77077dce77c87a310f76
Guru <guru@duck.com>
2023-07-01 04:43:12 +0530

    Update after-upgrade.sh

--- after-upgrade.sh
+++ after-upgrade.sh
@@ -1,40 +1,26 @@
-adb shell pm install-existing --user 0 com.android.mms.service && adb shell pm enable --user 0 com.android.mms.service
-adb shell pm install-existing --user 0 com.coloros.athena && adb shell pm enable --user 0 com.coloros.athena
-adb shell pm install-existing --user 0 com.coloros.calculator && adb shell pm enable --user 0 com.coloros.calculator
-adb shell pm install-existing --user 0 com.coloros.encryption && adb shell pm enable --user 0 com.coloros.encryption
-adb shell pm install-existing --user 0 com.coloros.focusmode && adb shell pm enable --user 0 com.coloros.focusmode
-adb shell pm install-existing --user 0 com.coloros.gamespace && adb shell pm enable --user 0 com.coloros.gamespace
-adb shell pm install-existing --user 0 com.coloros.logkit && adb shell pm enable --user 0 com.coloros.logkit
-adb shell pm install-existing --user 0 com.coloros.oppomultiapp && adb shell pm enable --user 0 com.coloros.oppomultiapp
-adb shell pm install-existing --user 0 com.coloros.providers.downloads.ui && adb shell pm enable --user 0 com.coloros.providers.downloads.ui
-adb shell pm install-existing --user 0 com.coloros.sau && adb shell pm enable --user 0 com.coloros.sau
-adb shell pm install-existing --user 0 com.coloros.securitykeyboard && adb shell pm enable --user 0 com.coloros.securitykeyboard
-adb shell pm install-existing --user 0 com.coloros.translate.engine && adb shell pm enable --user 0 com.coloros.translate.engine
-adb shell pm install-existing --user 0 com.daemon.shelper && adb shell pm enable --user 0 com.daemon.shelper
-adb shell pm install-existing --user 0 com.google.android.accessibility.soundamplifier && adb shell pm enable --user 0 com.google.android.accessibility.soundamplifier
-adb shell pm install-existing --user 0 com.google.android.apps.nbu.paisa.user && adb shell pm enable --user 0 com.google.android.apps.nbu.paisa.user
-adb shell pm install-existing --user 0 com.google.android.configupdater && adb shell pm enable --user 0 com.google.android.configupdater
-adb shell pm install-existing --user 0 com.google.android.contacts && adb shell pm enable --user 0 com.google.android.contacts
-adb shell pm install-existing --user 0 com.google.android.ims && adb shell pm enable --user 0 com.google.android.ims
-adb shell pm install-existing --user 0 com.google.android.inputmethod.latin && adb shell pm enable --user 0 com.google.android.inputmethod.latin
-adb shell pm install-existing --user 0 com.google.android.syncadapters.contacts && adb shell pm enable --user 0 com.google.android.syncadapters.contacts
-adb shell pm install-existing --user 0 com.heytap.openid && adb shell pm enable --user 0 com.heytap.openid
-adb shell pm install-existing --user 0 com.heytap.synergy && adb shell pm enable --user 0 com.heytap.synergy
-adb shell pm install-existing --user 0 com.heytap.themestore && adb shell pm enable --user 0 com.heytap.themestore
-adb shell pm install-existing --user 0 com.nearme.atlas && adb shell pm enable --user 0 com.nearme.atlas
-adb shell pm install-existing --user 0 com.nearme.romupdate && adb shell pm enable --user 0 com.nearme.romupdate
-adb shell pm install-existing --user 0 com.nearme.statistics.rom && adb shell pm enable --user 0 com.nearme.statistics.rom
-adb shell pm install-existing --user 0 com.oplus.encryption && adb shell pm enable --user 0 com.oplus.encryption
-adb shell pm install-existing --user 0 com.oplus.multiapp && adb shell pm enable --user 0 com.oplus.multiapp
-adb shell pm install-existing --user 0 com.oplus.pay && adb shell pm enable --user 0 com.oplus.pay
-adb shell pm install-existing --user 0 com.oplus.romupdate && adb shell pm enable --user 0 com.oplus.romupdate
-adb shell pm install-existing --user 0 com.oplus.sau && adb shell pm enable --user 0 com.oplus.sau
-adb shell pm install-existing --user 0 com.opos.cs && adb shell pm enable --user 0 com.opos.cs
-adb shell pm install-existing --user 0 com.oppo.lfeh && adb shell pm enable --user 0 com.oppo.lfeh
-adb shell pm install-existing --user 0 com.oppo.nhs && adb shell pm enable --user 0 com.oppo.nhs
-adb shell pm install-existing --user 0 com.oppo.operationManual && adb shell pm enable --user 0 com.oppo.operationManual
-adb shell pm install-existing --user 0 com.oppo.qualityprotect && adb shell pm enable --user 0 com.oppo.qualityprotect
-adb shell pm install-existing --user 0 com.oppoex.afterservice && adb shell pm enable --user 0 com.oppoex.afterservice
-adb shell pm install-existing --user 0 com.qti.snapdragon.qdcm_ff && adb shell pm enable --user 0 com.qti.snapdragon.qdcm_ff
+adb shell pm install-existing --user 0 com.android.mms.service
+adb shell pm install-existing --user 0 com.coloros.athena
+adb shell pm install-existing --user 0 com.coloros.calculator
+adb shell pm install-existing --user 0 com.coloros.encryption
+adb shell pm install-existing --user 0 com.coloros.focusmode
+adb shell pm install-existing --user 0 com.coloros.gamespace
+adb shell pm install-existing --user 0 com.coloros.providers.downloads.ui
+adb shell pm install-existing --user 0 com.coloros.sau
+adb shell pm install-existing --user 0 com.coloros.translate.engine
+adb shell pm install-existing --user 0 com.daemon.shelper
+adb shell pm install-existing --user 0 com.google.android.accessibility.soundamplifier
+adb shell pm install-existing --user 0 com.google.android.apps.nbu.paisa.user
+adb shell pm install-existing --user 0 com.google.android.configupdater
+adb shell pm install-existing --user 0 com.google.android.contacts
+adb shell pm install-existing --user 0 com.google.android.ims
+adb shell pm install-existing --user 0 com.google.android.inputmethod.latin
+adb shell pm install-existing --user 0 com.google.android.syncadapters.contacts
+adb shell pm install-existing --user 0 com.heytap.openid
+adb shell pm install-existing --user 0 com.nearme.romupdate
+adb shell pm install-existing --user 0 com.oplus.romupdate
+adb shell pm install-existing --user 0 com.oplus.sau
+adb shell pm install-existing --user 0 com.opos.cs
+adb shell pm install-existing --user 0 com.oppoex.afterservice
+adb shell pm install-existing --user 0 com.qti.snapdragon.qdcm_ff
 
 set /p exitkey= "Press any key to continue..."
